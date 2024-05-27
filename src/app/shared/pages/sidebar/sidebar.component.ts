73ae2193fc0cb1857426a5e91f8f23ea7da48e87
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-05-26 20:54:02 -0600

    base routes

---
+++ src/app/shared/pages/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+
+  routes = [
+    {
+      path: 'home', 
+    },
+    {
+      path: 'about', 
+    },
+    {
+      path: 'contact',
+    }
+  ]
+}
